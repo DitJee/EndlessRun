0d34dcad7bb9d9f955f6cf446564ee0f41f9ca20
dit dejphachon <ditdejphachon@gmail.com>
2021-11-10 15:14:06 +0700

    added Floor tile generation

--- Source/EndlessRun/EndlessRunGameModeBase.cpp
+++ Source/EndlessRun/EndlessRunGameModeBase.cpp
@@ -3,3 +3,28 @@
 
 #include "EndlessRunGameModeBase.h"
 
+void AEndlessRunGameModeBase::CreateInitialFloorTiles()
+{
+	for (int i = 0; i < NumInitialFloorTiles; i++)
+	{
+		AddFloorTiles();
+	}
+}
+
+void AEndlessRunGameModeBase::AddFloorTiles()
+{
+	UWorld* World = GetWorld();
+	if (World) {
+		AFloorTile* Tile = World->SpawnActor<AFloorTile>(FloorTileClass, NextSpawnPoint);
+
+		if (Tile) {
+			NextSpawnPoint = Tile->GetAttachTransform();
+		}
+	}
+	
+}
+
+void AEndlessRunGameModeBase::BeginPlay()
+{
+	CreateInitialFloorTiles();
+}
--- Source/EndlessRun/EndlessRunGameModeBase.h
+++ Source/EndlessRun/EndlessRunGameModeBase.h
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
-
+#include "FloorTile.h"
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "EndlessRunGameModeBase.generated.h"
@@ -14,4 +14,24 @@ class ENDLESSRUN_API AEndlessRunGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditAnywhere, Category = "Config")
+		TSubclassOf<AFloorTile> FloorTileClass;
+
+	UPROPERTY(EditAnywhere,  Category = "Config")
+		int32 NumInitialFloorTiles = 10;
+
+	UPROPERTY(VisibleInstanceOnly, Category = "RunTine")
+		FTransform NextSpawnPoint;
+
+	UFUNCTION(BlueprintCallable)
+		void CreateInitialFloorTiles();
+
+	UFUNCTION(BlueprintCallable)
+		void AddFloorTiles();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 };
